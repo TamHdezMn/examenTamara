14a450323f280c985a0486217b1946742718ac21
Tamara Hernandez Marin <tamara.hernandez@a.jrotero.es>
2025-03-15 09:10:42 +0100

    comentario en develop

---
+++ practicaDocker/src/test/java/com/es/practica/PracticaDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.es.practica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticaDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
